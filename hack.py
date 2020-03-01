9f1f9a6c49326af70a18a5e1425bbf26854bce05
Lucas James <ljgecko1230@gmail.com>
2020-03-01 01:34:02 -0800

    added multiple level support

--- hack.py
+++ hack.py
@@ -42,6 +42,13 @@ class File:
         else:
             print("This is not a locked file.")
 
+class Goal:
+    def __init__(self, name, contents):
+        self.name = name
+        self.contents = contents
+
+        self.is_locked = False
+
 def clear_screen():
     os.system("cls")
 
@@ -105,51 +112,67 @@ def read_file(file):
 
     print("That file does not exist.")
 
+def extract_file(file):
+    for item in current_directory.contents:
+        if item.name == file:
+            if type(item) == Goal:
+                level_completed = True
+
+def run():
+    global current_directory
+
+    level_completed = False
+    clear_screen()
+
+    while True:
+        try:
+            current_directory = file_system[current_directory_path]
+
+            command = input(str(current_directory_path) + " - $ ").strip()
+
+            if command == "cls":
+                clear_screen()
+
+            elif command == "ls":
+                list_contents()
+
+            elif command.startswith("cd"):
+                split_command = command.split(" ")
+                change_directory(split_command[1])
+
+            elif command.startswith("unlock"):
+                split_command = command.split(" ")
+                file_system[current_directory_path + "/" + split_command[1]].unlock()
+
+            elif command.startswith("read"):
+                split_command = command.split(" ")
+                read_file(split_command[1])
+
+            elif command.startswith("extract"):
+                split_command = command.split(" ")
+                extract_file(split_command[1])
+
+            if command == "quit":
+                sys.exit()
+
+        except:
+            print("Invalid syntax or name. Please try again.")
+
+        print()
+
+        if level_completed:
+            return
+
+current_directory = None
+
+# Level 1
 file_system = {}
-
 file_system["/home"] = Directory("home", False)
-
 file_system["/home/test_1"] = Directory("test_1", True, "1234")
 file_system["/home"].contents.append(file_system["/home/test_1"])
-
 file_system["/home/test_1/password.txt"] = File("password.txt", "The password for test_2 is: 'p.><*as?swor/d'", False)
 file_system["/home/test_1"].contents.append(file_system["/home/test_1/password.txt"])
-
 file_system["/home/test_1/test_2"] = Directory("test_2", True, "p.><*as?swor/d")
 file_system["/home/test_1"].contents.append(file_system["/home/test_1/test_2"])
-
 current_directory_path = "/home"
-
-clear_screen()
-
-while True:
-    try:
-        current_directory = file_system[current_directory_path]
-
-        command = input(str(current_directory_path) + " - $ ").strip()
-
-        if command == "cls":
-            clear_screen()
-
-        elif command == "ls":
-            list_contents()
-
-        elif command.startswith("cd"):
-            split_command = command.split(" ")
-            change_directory(split_command[1])
-
-        elif command.startswith("unlock"):
-            split_command = command.split(" ")
-            file_system[current_directory_path + "/" + split_command[1]].unlock()
-
-        elif command.startswith("read"):
-            split_command = command.split(" ")
-            read_file(split_command[1])
-
-        if command == "quit":
-            break
-
-    except:
-        print("Invalid syntax or name. Please try again.")
-
-    print()
+run()
